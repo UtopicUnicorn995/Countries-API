2780862c9feacc69f3075fc52c63308c2f934a81
utopicUnicorn <christianbarrios995@gmail.com>
2023-04-11 10:28:06 +0800

    updated this

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,35 +1,55 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import CountryCard from "./CountryCard";
 
-function Cards(){
-    const getData = () => {
-        fetch('data.json', {
-            headers: {
-                "Content-Type": 'application/json',
-                "Accept": 'application/json'
-            }
-        }).then(function(response){
-            console.log(response)
-            return response.json()
-        }).then(function(data){
-            console.log(data)
-        })
-    }
+function Cards() {
+  const [countries, setCountries] = useState([]);
 
-    useEffect(() => {
-        getData()
-    }, [])
+  console.log(countries);
 
-    return(
-        <section className="card-section">
-            <div className="card">
-                <img src="" alt="" />
-                <p className="country"></p>
-                <p></p>
-                <p></p>
-                <p></p>
+  const getData = () => {
+    fetch("data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (datas) {
+        // const dataCard = datas.map((data) => {
+        //   return <CountryCard key={data.name} countryProp={data} />;
+        // });
+        // setCountry(dataCard);
+        setCountries(datas);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <section className="card-section">
+      {countries.map((country) => {
+        console.log(country);
+        return (
+          <div className="card">
+            <img className="card--img" src={country.flag} alt="" />
+            <div className="card--text-container p-4">
+              <p className="country-name fw-bolder">{country.name}</p>
+              <p className="fs-sm mb-0">
+                Population: {country.population.toLocaleString("en-US")}
+              </p>
+              <p className="fs-sm mb-0">Region: {country.region}</p>
+              <p className="fs-sm mb-0">Capital: {country.capital}</p>
             </div>
-        </section>
-    )
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 
-export default Cards
+export default Cards;
---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+
+export default function CountryCard(props) {
+  console.log(props);
+}
